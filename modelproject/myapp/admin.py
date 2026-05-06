2f7d2bac0bb96f9647001c7ed85f7dfd62bde1a9
SuprithKumar20 <suprithkumar2021@gmail.com>
2026-05-06 14:58:18 +0530

    added modelproject

--- modelproject/myapp/admin.py
+++ modelproject/myapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from myapp.models import Student
 # Register your models here.
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=['name','age','gender','dob','email']
+admin.site.register(Student,StudentAdmin)
